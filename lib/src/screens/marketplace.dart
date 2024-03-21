9bbbfdc0aa7c7ec00d9231a5bd9454966f5065c5
TheSmartBeaver <totorx36bhu@gmail.com>
2024-03-21 23:39:58 +0100

    test connectivité pour afficher marketplace

--- lib/src/screens/marketplace.dart
+++ lib/src/screens/marketplace.dart
@@ -1,5 +1,6 @@
 import 'package:beaver_learning/src/widgets/shared/app_drawer.dart';
 import 'package:beaver_learning/src/widgets/shared/widgets/CustomDropdown.dart';
+import 'package:connectivity/connectivity.dart';
 import 'package:flutter/material.dart';
 import 'package:beaver_learning/data/constants.dart';
 import 'package:transparent_image/transparent_image.dart';
@@ -23,6 +24,8 @@ class Course {
 }
 
 class _MarketPlaceScreenState extends State<MarketPlaceScreen> {
+  bool _isOnline = false;
+
   List<Course> courses = [
     Course("Apprends à parler Anglais", 29.99,
         "https://img.pixers.pics/pho_wat(s3:700/FO/40/70/37/94/700_FO40703794_f3c68522bf4a853d282896173902b992.jpg,450,700,cms:2018/10/5bd1b6b8d04b8_220x50-watermark.png,over,230,650,jpg)/coussins-decoratifs-drapeau-anglais-big-ben-verticale.jpg.jpg"),
@@ -39,54 +42,115 @@ class _MarketPlaceScreenState extends State<MarketPlaceScreen> {
   ];
 
   @override
+  void initState() {
+    super.initState();
+    _checkConnectivity();
+    Connectivity().onConnectivityChanged.listen((result) {
+      if (result != ConnectivityResult.none) {
+        setState(() {
+          _isOnline = true;
+        });
+      } else {
+        setState(() {
+          _isOnline = false;
+        });
+      }
+    });
+  }
+
+  Future<void> _checkConnectivity() async {
+    var connectivityResult = await (Connectivity().checkConnectivity());
+    if (connectivityResult == ConnectivityResult.none) {
+      setState(() {
+        _isOnline = false;
+      });
+    } else {
+      setState(() {
+        _isOnline = true;
+      });
+    }
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(title: Text(widget.title)),
-        body: GridView.builder(
-            padding: const EdgeInsets.all(8.0),
-            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-              crossAxisCount: 3,
-              childAspectRatio: 2 / 3,
-              crossAxisSpacing: 10,
-              mainAxisSpacing: 10,
-            ),
-            itemCount: courses.length,
-            itemBuilder: (context, index) {
-              return Card(
-                child: Column(
-                  children: [
-                    Column(
+        appBar: AppBar(title: const Text("Marketplace")),
+        body: _isOnline
+            ? GridView.builder(
+                padding: const EdgeInsets.all(8.0),
+                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 3,
+                  childAspectRatio: 2 / 3,
+                  crossAxisSpacing: 10,
+                  mainAxisSpacing: 10,
+                ),
+                itemCount: courses.length,
+                itemBuilder: (context, index) {
+                  return Card(
+                    child: Column(
                       children: [
-                        Text(courses[index].name, style: const TextStyle(fontWeight: FontWeight.bold, overflow: TextOverflow.visible)),
+                        Column(
+                          children: [
+                            Text(courses[index].name,
+                                style: const TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                    overflow: TextOverflow.visible)),
+                          ],
+                        ),
+                        const SizedBox(height: 4),
+                        Expanded(
+                          child: FadeInImage(
+                              placeholder: MemoryImage(kTransparentImage),
+                              image: NetworkImage(courses[index].photoUrl),
+                              fit: BoxFit.cover),
+                        ),
+                        Container(
+                          margin: const EdgeInsets.all(4),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              GestureDetector(
+                                  onTap: () {
+                                    showDialog(
+                                        context: context,
+                                        builder: (context) => AlertDialog(
+                                              title: const Text("Whooops !"),
+                                              content: const Text(
+                                                  'Ce cours est actuellement en cours de développement. Veuillez réessayer plus tard !'),
+                                              actions: [
+                                                TextButton(
+                                                  onPressed: () {
+                                                    Navigator.pop(context);
+                                                  },
+                                                  child: const Text('Okay'),
+                                                ),
+                                              ],
+                                            ));
+                                  },
+                                  child: const Icon(
+                                    Icons.remove_red_eye_sharp,
+                                    color: Colors.deepOrange,
+                                  )),
+                              Text('${courses[index].price} €',
+                                  style: const TextStyle(
+                                      color: Colors.purpleAccent,
+                                      fontWeight: FontWeight.bold)),
+                            ],
+                          ),
+                        )
                       ],
                     ),
-                    const SizedBox(height: 4),
-                    Expanded(
-                      child: FadeInImage(
-                          placeholder: MemoryImage(kTransparentImage),
-                          image: NetworkImage(courses[index].photoUrl),
-                          fit: BoxFit.cover),
-                    ),
-                    Container(
-                      margin: const EdgeInsets.all(4),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          const Icon(
-                            Icons.remove_red_eye_sharp,
-                            color: Colors.deepOrange,
-                          ),
-                          Text('${courses[index].price} €',
-                              style: const TextStyle(
-                                  color: Colors.purpleAccent,
-                                  fontWeight: FontWeight.bold)),
-                        ],
-                      ),
-                    )
+                  );
+                })
+            : const Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Text('Pas de connexion Internet'),
+                    Text('Veuillez activer votre connexion Internet'),
                   ],
                 ),
-              );
-            }),
+              ),
         drawer: const AppDrawer());
   }
 }
