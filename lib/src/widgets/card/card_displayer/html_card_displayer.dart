ed5fec8464967a8c9a3410a72d5c734b00a757c8
TheSmartBeaver <totorx36bhu@gmail.com>
2024-11-12 12:17:52 +0100

    transition to flutter_inappwebview

--- lib/src/widgets/card/card_displayer/html_card_displayer.dart
+++ lib/src/widgets/card/card_displayer/html_card_displayer.dart
@@ -1,14 +1,13 @@
-import 'dart:io';
-
 import 'package:beaver_learning/src/dao/html_dao.dart';
-import 'package:beaver_learning/src/dao/image_dao.dart';
-import 'package:beaver_learning/src/models/db/database.dart';
 import 'package:beaver_learning/src/models/db/databaseInstance.dart';
 import 'package:beaver_learning/src/utils/images_functions.dart';
 import 'package:beaver_learning/src/utils/template_functions.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_tts/flutter_tts.dart';
 import 'package:path_provider/path_provider.dart';
+import 'package:translator/translator.dart';
 import 'package:webview_flutter/webview_flutter.dart';
+import 'package:flutter_inappwebview/flutter_inappwebview.dart';
 
 WebViewController _buildController() {
   return WebViewController()
@@ -32,8 +31,51 @@ WebViewController _buildController() {
         },
       ),
     );
-  //..loadRequest(Uri.parse('https://flutter.dev'));
-  //..loadHtmlString(customHtml);
+}
+
+void _buildInAppController(InAppWebViewController webViewController) {
+  webViewController.setOptions(
+      options: InAppWebViewGroupOptions(
+    crossPlatform: InAppWebViewOptions(
+      javaScriptEnabled: true,
+      clearCache: true,
+      useShouldOverrideUrlLoading: true,
+      useOnLoadResource: true,
+      useOnDownloadStart: true,
+    ),
+    android: AndroidInAppWebViewOptions(
+      useHybridComposition: true,
+    ),
+    ios: IOSInAppWebViewOptions(
+      allowsInlineMediaPlayback: true,
+    ),
+  ));
+
+  webViewController.addJavaScriptHandler(
+    handlerName: 'ttsHandler',
+    callback: (args) async {
+      // Utile pour plus tard dès que je voudrais mettre à jour des cartes pour les traduires automatiquement !
+      // final translator = GoogleTranslator();
+      // translator.translate(args[0], from: 'fr', to: 'en').then(print);
+
+      String targetLanguage = args[0];
+      String textToSpeak = args[1];
+
+      FlutterTts flutterTts = FlutterTts();
+      await flutterTts.awaitSpeakCompletion(true);
+      await flutterTts.awaitSynthCompletion(true);
+
+      List<dynamic> languages = await flutterTts.getLanguages;
+      await flutterTts.setLanguage(targetLanguage);
+      await flutterTts.setSpeechRate(0.5);
+      await flutterTts.setVolume(1.0);
+      await flutterTts.setPitch(1.0);
+      //var isLanguageAvailable = await flutterTts.isLanguageAvailable("en-US");
+
+      var aahahah = await flutterTts.speak(textToSpeak);
+      var toto = 0;
+    },
+  );
 }
 
 class HTMLCardDisplayer extends StatefulWidget {
@@ -55,19 +97,13 @@ class HTMLCardDisplayer extends StatefulWidget {
 }
 
 class _HTMLCardDisplayerState extends State<HTMLCardDisplayer> {
-  WebViewController controller = _buildController();
+  //WebViewController controller = _buildController();
+  late InAppWebViewController webViewController;
   late String recto;
   late String verso;
-  // https://img-4.linternaute.com/s-R4pIq7BydGotKb7_D83E_ViXM=/1500x/smart/fde5024007754647b344329e6a8ca64b/ccmcms-linternaute/33631821.jpg
-  // var recto =
-  //       "<ul><li>Hello, World!</li><li>Pourquoi les vampires ne peuvent-ils pas être de bons photographes</li></ul>";
-  //   var verso =
-  //       "<ul><li>Salut, le monde !</li><li>Parce qu'ils ont peur d'être \"développés\" par le soleil !</li><li><img width='600' height='400' src=\"batman2.png\" /></li></ul>";
-
+  late String initialUrl;
   void refresh() {
-    setState(() {
-      // Besoin de rien juste d'un nouveau get en BDD du recto et verso
-    });
+    setState(() {});
   }
 
   Future<void> init() async {
@@ -84,18 +120,15 @@ class _HTMLCardDisplayerState extends State<HTMLCardDisplayer> {
         customHtmlString, target_file, content.files);
     var files = await listFiles(await getTemporaryDirectory());
     //await controller.clearCache();
-    await controller.clearLocalStorage();
-    await controller.loadRequest(Uri.parse('$localServerUrl/$target_file'));
+    //await controller.clearLocalStorage();
+    //await controller.loadRequest(Uri.parse('$localServerUrl/$target_file'));
+    //webViewController.clearCache();
+    initialUrl = '$localServerUrl/$target_file';
     var test2 = await (await getTemporaryDirectory()).list().toList();
-    //await controller.loadHtmlString(_getCustomHtml(recto, verso, widget.isPrintAnswer));
-    //serverProps.server.close(force: true);
   }
 
   @override
   Widget build(BuildContext context) {
-    // controller
-    //     .loadHtmlString(_getCustomHtml(recto, verso, widget.isPrintAnswer));
-
     return Column(children: [
       Expanded(
           child: FutureBuilder(
@@ -104,7 +137,37 @@ class _HTMLCardDisplayerState extends State<HTMLCardDisplayer> {
                 if (snapshot.connectionState == ConnectionState.waiting) {
                   return const CircularProgressIndicator();
                 } else {
-                  return WebViewWidget(controller: controller);
+                  return InAppWebView(
+                    initialUrlRequest: URLRequest(url: Uri.parse(initialUrl)),
+                    onWebViewCreated: (controller) {
+                      _buildInAppController(controller);
+                      webViewController = controller;
+                    },
+                    onLoadStart: (controller, url) {
+                      print("Started loading: $url");
+                    },
+                    onLoadStop: (controller, url) async {
+                      print("Finished loading: $url");
+                    },
+                    onProgressChanged: (controller, progress) {
+                      // Update loading bar.
+                    },
+                    onConsoleMessage: (controller, consoleMessage) {
+                      print("Console message: ${consoleMessage.message}");
+                    },
+                    onLoadError: (controller, url, code, message) {
+                      print(
+                          "Error loading: $url, code: $code, message: $message");
+                    },
+                    shouldOverrideUrlLoading:
+                        (controller, navigationAction) async {
+                      var uri = navigationAction.request.url!;
+                      if (uri.toString().startsWith('http://localhost')) {
+                        return NavigationActionPolicy.ALLOW;
+                      }
+                      return NavigationActionPolicy.CANCEL;
+                    },
+                  );
                 }
               }))
     ]);
